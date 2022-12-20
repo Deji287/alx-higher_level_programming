8d7f8eb3ef58e73900729833951f864d4681c94a
Deji287 <slimdejavu0101@gmail.com>
2022-12-20 04:26:14 -0800

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,31 @@
--- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
-USE hbtn_0e_4_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+'''task 4 script'''
 
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+import MySQLdb
+import sys
+
+
+def list_all():
+    '''lists all cities from db'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute('SELECT c.id, c.name, s.name FROM cities c LEFT ' +
+                'JOIN states s ON c.state_id = s.id ORDER BY c.id ASC;')
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+
+    if result:
+        for row in result:
+            print(row)
+
+
+if __name__ == '__main__':
+    list_all()
